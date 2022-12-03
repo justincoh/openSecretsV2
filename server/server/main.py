17d5b2415bc5ee7a29d059e28324af8b8a98a242
justin cohen <justincoh@gmail.com>
2022-12-03 13:41:16 -0500

    add route names/scaffolding

--- server/server/main.py
+++ server/server/main.py
@@ -2,6 +2,34 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+# Might be able to do this entirely without a DB
+# Just create the files you need and load them up at module import time
+# and keep in memory, shouldn't take up too much space this data isn't large
+# https://www.pythonanywhere.com/forums/topic/27680/
+
+
+@app.route("/", methods=["GET"])
 def hello_world():
+    # Do we want to use jinja?
     return "<p>Hello, World!</p>"
+
+
+@app.route("/states", methods=["GET"])
+def state_summaries():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/states/<state_code>", methods=["GET"])
+def state_by_code(state_code):
+    print(f"the state code {state_code}")
+    return f"<p>Hello, {state_code}!</p>"
+
+
+@app.route("/reps", methods=["GET"])
+def list_representatives():
+    return f"<p>Hello, list_representatives</p>"
+
+
+@app.route("/reps/<cid>", methods=["GET"])
+def get_rep_by_cid(cid):
+    return f"<p>candidate id: {cid}</p>"
